7ab619d0dc964181728f139d15260b40c7d0431d
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2021-01-08 13:30:54 +0200

    Update webpush.php

--- config/webpush.php
+++ config/webpush.php
@@ -2,7 +2,7 @@
 
 return [
 
-    /*
+    /**
      * These are the keys for authentication (VAPID).
      * These keys must be safely stored and should not change.
      */
@@ -13,30 +13,30 @@ return [
         'pem_file' => env('VAPID_PEM_FILE'),
     ],
 
-    /*
-     * Google Cloud Messaging.
-     * Deprecated and optional. It's here only for compatibility reasons.
-     */
-    'gcm' => [
-        'key' => env('GCM_KEY'),
-        'sender_id' => env('GCM_SENDER_ID'),
-    ],
-
-    /*
+    /**
      * This is model that will be used to for push subscriptions.
      */
     'model' => \NotificationChannels\WebPush\PushSubscription::class,
 
-    /*
+    /**
      * This is the name of the table that will be created by the migration and
      * used by the PushSubscription model shipped with this package.
      */
     'table_name' => env('WEBPUSH_DB_TABLE', 'push_subscriptions'),
 
-    /*
+    /**
      * This is the database connection that will be used by the migration and
      * the PushSubscription model shipped with this package.
      */
     'database_connection' => env('WEBPUSH_DB_CONNECTION', env('DB_CONNECTION', 'mysql')),
+
+    /**
+     * Google Cloud Messaging.
+     * @deprecated
+     */
+    'gcm' => [
+        'key' => env('GCM_KEY'),
+        'sender_id' => env('GCM_SENDER_ID'),
+    ],
 
 ];
